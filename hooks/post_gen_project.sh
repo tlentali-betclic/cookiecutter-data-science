4d310868f4474312c74243d8d4f5424a556461a9
paul <paul.steff@hotmail.fr>
2021-08-30 18:07:23 +0200

    add github infos on cookiecutter

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,46 +1,15 @@
 #!/bin/bash
 
 # taken from https://www.viget.com/articles/create-a-github-repo-from-the-command-line
-repo_name=$1
 
-dir_name=`basename $(pwd)`
+repo_name = '{{ cookiecutter.repo_name }}'
 
+username='{{ cookiecutter.github_username }}'
 
-if [ "$repo_name" = "" ]; then
-    echo "Repo name (hit enter to use '$dir_name')?"
-    read repo_name
-fi
-
-if [ "$repo_name" = "" ]; then
-repo_name=$dir_name
-fi
-
-username=`git config github.user`
-if [ "$username" = "" ]; then
-    echo "GitHub username?"
-    read username
-    if [ "$username" = "" ]; then
-      echo "Could not find username, run 'git config --global github.user <username>'"
-      invalid_credentials=1
-    fi
-fi
-
-token=`git config github.token`
-if [ "$token" = "" ]; then
-    echo "GitHub token?"
-    read token
-    if [ "$username" = "" ]; then
-      echo "Could not find token, run 'git config --global github.token <token>'"
-      invalid_credentials=1
-    fi
-fi
-
-if [ "$invalid_credentials" == "1" ]; then
-    exit 1
-fi
+token='{{ cookiecutter.github_token }}'
 
 echo -n "Creating Github repository '$repo_name' ..."
-curl -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'", "private":true}' > /dev/null 2>&1
+# curl -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'", "private":true}' > /dev/null 2>&1
 echo " done."
 
 # echo "Pushing local code to remote ..."
