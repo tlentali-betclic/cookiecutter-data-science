3e225669f79d12d53dee4812c38f7fc704fdabdf
Paul Steffen <p.steffen@betclicgroup.com>
2022-02-22 16:08:03 +0100

    first commit for betclic version

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,65 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-REMOVE_PATHS = [
+import logging
+import os
+import shutil
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("post_gen_project")
+
+DOCS_SOURCES = "docs_sources"
+ALL_TEMP_FOLDERS = [DOCS_SOURCES]
+DOCS_FILES_BY_TOOL = {
+    "mkdocs": ["index.md", "/mkdocs.yml", "betclic_logo.png"],
+    "sphinx": ["conf.py", "index.rst", "make.bat", "Makefile"],
+}
+TEMP_PATHS = [
     '{% if cookiecutter.dependency_management_tool != "pip" %} requirements.txt {% endif %}',
     '{% if cookiecutter.dependency_management_tool != "poetry" %} poetry.lock {% endif %}',
+    '{% if cookiecutter.data_quality_checker != "true" %} great_expectations {% endif %}'
 ]
 
-for path in REMOVE_PATHS:
-    path = path.strip()
-    if path and os.path.exists(path):
-        if os.path.isdir(path):
-            os.rmdir(path)
-        else:
-            os.unlink(path)
+
+def move_docs_files(docs_tool, docs_files, docs_sources):
+    if docs_tool == "none":
+        return
+
+    root = os.getcwd()
+    docs = "docs"
+
+    logger.info("Initializing docs for %s", docs_tool)
+    if not os.path.exists(docs):
+        os.mkdir(docs)
+
+    for item in docs_files[docs_tool]:
+        dst, name = (root, item[1:]) if item.startswith("/") else (docs, item)
+        src_path = os.path.join(docs_sources, docs_tool, name)
+        dst_path = os.path.join(dst, name)
+
+        logger.info("Moving %s to %s.", src_path, dst_path)
+        if os.path.exists(dst_path):
+            os.unlink(dst_path)
+
+        os.rename(src_path, dst_path)
+
+
+def remove_temp_folders(temp_folders):
+    for folder in temp_folders:
+        logger.info("Remove temporary folder: %s", folder)
+        shutil.rmtree(folder)
+
+
+def remove_temp_paths(temp_paths):
+    for path in temp_paths:
+        path = path.strip()
+        if path and os.path.exists(path):
+            if os.path.isdir(path):
+                os.rmdir(path)
+            else:
+                os.unlink(path)
+
+if __name__ == "__main__":
+    move_docs_files("{{cookiecutter.doc_tool}}", DOCS_FILES_BY_TOOL, DOCS_SOURCES)
+    remove_temp_folders(ALL_TEMP_FOLDERS)
+    remove_temp_paths(TEMP_PATHS)
---
+++ {{ cookiecutter.repo_name }}/docs_sources/mkdocs/index.md
@@ -0,0 +1,3 @@
+# Welcome to {{cookiecutter.repo_name}}
+
+{{cookiecutter.description}}
